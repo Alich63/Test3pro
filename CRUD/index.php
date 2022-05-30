85cac8a5c6b91b3ddbc032969a6d3ee265bdd168
Alich63 <chali786m@gmail.com>
2022-05-30 09:03:59 -0700

    sahara
    sjsjs

--- CRUD/index.php
+++ CRUD/index.php
@@ -29,6 +29,7 @@ if(isset($_REQUEST['sub']))
 <body>
     Hi my name is Sharafat ALi
     ANOTHER WAY UD
+    another jjjjjjjj
     <form action="" method="post">
         NAME
         <input type="text" name="nam" value="" id="" placeholder="Name" required><br>
